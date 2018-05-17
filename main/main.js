9fa79e64ae5a73ee1e650d49ce5539c9529c9436
Jonathan Boiser <jboiser@gmail.com>
2018-05-17 12:41:31 -0700

    reproduce error

---
+++ main/main.js
@@ -0,0 +1,14 @@
+// Error
+// 
+// /Users/jon/github/simple-repo/lib.js:1 Uncaught (in promise) SyntaxError: Unexpected token export
+//     at Module.get_Module._compile (/Applications/Atom.app/Contents/Resources/app/src/native-compile-cache.js:94:52)
+//     at Object.value [as .js] (/Applications/Atom.app/Contents/Resources/app/src/compile-cache.js:240:29)
+//     at Module.load (module.js:488:32)
+//     at tryModuleLoad (module.js:447:12)
+//     at Function.Module._load (module.js:439:3)
+//     at Module.require (file:///Applications/Atom.app/Contents/Resources/app.asar/static/index.js:47:45)
+//     at require (/Applications/Atom.app/Contents/Resources/app/src/native-compile-cache.js:66:33)
+//     at lookupCommonJs (/Users/jon/.atom/packages/autocomplete-modules/src/utils/export-module-completion.js:88:23)
+//     at parseFile.then.catch (/Users/jon/.atom/packages/autocomplete-modules/src/utils/export-module-completion.js:82:14)
+//     at <anonymous>
+import { foo } from '../lib';
